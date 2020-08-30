559cf812a0ad9d6be13fafd3f29f63749f1d00c4
Alex Lynch <mlynch5187@gmail.com>
2020-08-30 10:45:12 -0600

    Add factory association between item and merchant

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,9 +2,13 @@ FactoryBot.define do
   factory :item do
     name { "MyString" }
     description { "MyString" }
-    unit_price { 1.5 }
+    unit_price { 1.7 }
     updated_at { "MyString" }
     created_at { "MyString" }
     merchant_id { nil }
+  end
+
+  factory :post do
+    association :item, factory: :merchant
   end
 end
