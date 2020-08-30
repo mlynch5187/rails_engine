2b8a814d69f3bce9aedee91acc2b9a477ef6bc38
Alex Lynch <mlynch5187@gmail.com>
2020-08-30 10:42:12 -0600

    Add merchant item test to merchant_spec

--- spec/features/api/v1/merchant_spec.rb
+++ spec/features/api/v1/merchant_spec.rb
@@ -18,4 +18,22 @@ describe 'Merchants' do
       expect(merchant).to have_value({ 'name' => 'MyString' })
     end
   end
+
+  it 'can return merchant items' do
+    merchant = create(:merchant)
+    create_list(:item, 5, merchant: merchant)
+
+    get "/api/v1/merchants/#{merchant.id}/items"
+
+    items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(items['data'].count).to eq(5)
+    
+    items['data'].each do |item|
+      expect(item).to have_key('id')
+      expect(item).to have_key('type')
+      expect(item).to have_value('items')
+    end
+  end
 end
